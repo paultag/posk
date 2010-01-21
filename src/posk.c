13c17206af78abad6700f7ac4550322e1c27f1d4
PaulTag <PaulTag@0a7cd2d0-06b7-11df-b5c4-2942a6c37bad>
2010-01-21 20:48:59 +0000

    being awesome
    git-svn-id: https://posk.googlecode.com/svn/trunk@31 0a7cd2d0-06b7-11df-b5c4-2942a6c37bad

--- src/posk.c
+++ src/posk.c
@@ -1,10 +1,11 @@
 #include <posk/posk.h>
 #include <posk/malloc.h>
 #include <posk/posk_stdout.h>
+#include <sys/types.h>
 
 int main ( int argc, char ** argv ) {
-	char * foo = (char *)posk_malloc( sizeof(char) * 8 );
+	pchar * foo = (pchar *)posk_malloc( sizeof(pchar) * 8 );
 	foo = "Hello";
-	printf( "I was able to posk allocate %s\n", foo );
+	printf( "I was able to allocate and write %s\n", foo );
 	return 0;
 }
