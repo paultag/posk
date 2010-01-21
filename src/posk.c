689590bf69122289ce613950db8e0fbad052f6f9
PaulTag <PaulTag@0a7cd2d0-06b7-11df-b5c4-2942a6c37bad>
2010-01-21 22:00:43 +0000

    copyright stuff
    git-svn-id: https://posk.googlecode.com/svn/trunk@33 0a7cd2d0-06b7-11df-b5c4-2942a6c37bad

--- src/posk.c
+++ src/posk.c
@@ -4,6 +4,7 @@
 #include <sys/types.h>
 
 int main ( int argc, char ** argv ) {
+	copyright();
 	pchar * foo = (pchar *)posk_malloc( sizeof(pchar) * 8 );
 	foo = "Hello";
 	printf( "I was able to allocate and write %s\n", foo );
