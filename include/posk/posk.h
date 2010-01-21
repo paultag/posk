278dacfeeb4ab7f0f24a7282f5d6f0f4e5c1e17b
PaulTag <PaulTag@0a7cd2d0-06b7-11df-b5c4-2942a6c37bad>
2010-01-21 18:07:31 +0000

    adding in posk info
    git-svn-id: https://posk.googlecode.com/svn/trunk@4 0a7cd2d0-06b7-11df-b5c4-2942a6c37bad

---
+++ include/posk/posk.h
@@ -0,0 +1,1 @@
+#include "posk_info.h"
---
+++ include/posk/posk_info.h
@@ -0,0 +1,5 @@
+#define KERNEL_NAME "posk"
+#define KERNEL_COPYRIGHT "gpl"
+#define KERNEL_AUTHORS "Pat McDonnell, Paul Tagliamonte"
+#define KERNEL_YEAR "2010"
+
