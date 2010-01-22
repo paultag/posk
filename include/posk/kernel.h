04c200bb625f86fa90f7d530cebfe421eb8745e0
PaulTag <PaulTag@0a7cd2d0-06b7-11df-b5c4-2942a6c37bad>
2010-01-22 18:24:06 +0000

    adding log levels
    git-svn-id: https://posk.googlecode.com/svn/trunk@61 0a7cd2d0-06b7-11df-b5c4-2942a6c37bad

---
+++ include/posk/kernel.h
@@ -0,0 +1,12 @@
+/* Kernel Log Levels */
+
+/* Yes this is the same as linux. */
+
+#define KERN_EMERG           0
+#define KERN_ALERT           2
+#define KERN_CRIT            4
+#define KERN_ERROR           8
+#define KERN_WARNING         16
+#define KERN_NOTICE          32
+#define KERN_INFO            64
+#define KERN_DEBUG           128
