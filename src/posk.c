5d911d5cf99cddfe623c345dc7b665aae80ad08d
PaulTag <PaulTag@0a7cd2d0-06b7-11df-b5c4-2942a6c37bad>
2010-01-22 18:13:48 +0000

    vain
    git-svn-id: https://posk.googlecode.com/svn/trunk@59 0a7cd2d0-06b7-11df-b5c4-2942a6c37bad

--- src/posk.c
+++ src/posk.c
@@ -1,16 +1,17 @@
 #include <posk/posk.h>
 
+#include "slab.c"
 #include "term.c"
 
 void posk_start( void * mbd, unsigned int magic ) {
 	posk_clear_screen();
 	char foo[20];
 	char * bar = (char *)&foo;
-	bar = "Hello, World!\0";
+	bar = "Dynamic Text Support!!!! Paul Rules!\0";
 
 	int i = 0;
 
-	for ( ; i != 13; ++i ) {
+	for ( ; bar[i] != '\0'; ++i ) {
 		posk_print_char( i, 0, bar[i] );
 	}
 	
