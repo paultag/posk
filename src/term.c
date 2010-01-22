c792ba1792c7ca15c64c838c44a959c65cbda3ea
PaulTag <PaulTag@0a7cd2d0-06b7-11df-b5c4-2942a6c37bad>
2010-01-22 18:24:31 +0000

    bwah
    git-svn-id: https://posk.googlecode.com/svn/trunk@62 0a7cd2d0-06b7-11df-b5c4-2942a6c37bad

--- src/term.c
+++ src/term.c
@@ -7,18 +7,6 @@ void posk_print_char_tl( char c ) {
 	videoram[1] = WHITE_TXT; /* forground, background color. */
 }
 
-/*
-
-0 == 0
-1 == 2
-2 == 4
-3 == 6
-4 == 8
-5 == 10
-6 == 12
-
-*/
-
 void posk_print_char( int x, int y, char c ) {
 	unsigned char * videoram = (unsigned char *) 0xb8000;
 	int offset = x * 2 + ( y * MAX_WIDTH );
