7be059ecba6f496a5d6cef78619b80a30000bfc9
PaulTag <PaulTag@0a7cd2d0-06b7-11df-b5c4-2942a6c37bad>
2010-01-22 19:17:52 +0000

    small bounds issue
    git-svn-id: https://posk.googlecode.com/svn/trunk@63 0a7cd2d0-06b7-11df-b5c4-2942a6c37bad

--- src/posk.c
+++ src/posk.c
@@ -5,7 +5,7 @@
 
 void posk_start( void * mbd, unsigned int magic ) {
 	posk_clear_screen();
-	char foo[20];
+	char foo[30];
 	char * bar = (char *)&foo;
 	bar = "Dynamic Text Support!!!! Paul Rules!\0";
 
