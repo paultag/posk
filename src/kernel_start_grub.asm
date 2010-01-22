f54039dbb9bfeb4a984fc48ef5362a14a36b3b34
PaulTag <PaulTag@0a7cd2d0-06b7-11df-b5c4-2942a6c37bad>
2010-01-22 00:53:48 +0000

    fooing more
    git-svn-id: https://posk.googlecode.com/svn/trunk@36 0a7cd2d0-06b7-11df-b5c4-2942a6c37bad

---
+++ src/kernel_start_grub.asm
@@ -0,0 +1,28 @@
+%include "grub.inc" ; needed for the multiboot header
+
+[BITS 32]
+
+[global start]
+[extern posk_start] ; this is in the c file
+
+start:
+  call posk_start
+  cli
+  hlt
+
+
+; Multiboot header for GRUB bootloader. This must be in the first 8K
+; these are in the linker script file
+EXTERN code, bss, end
+
+ALIGN 4
+mboot:
+	dd MULTIBOOT_HEADER_MAGIC
+	dd MULTIBOOT_HEADER_FLAGS
+	dd MULTIBOOT_CHECKSUM
+; aout kludge. These must be PHYSICAL addresses
+	dd mboot
+	dd code
+	dd bss
+	dd end
+	dd start
