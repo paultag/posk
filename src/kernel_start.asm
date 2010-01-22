d6c4e18cc40e168be99afc7808e998983068a4ec
PaulTag <PaulTag@0a7cd2d0-06b7-11df-b5c4-2942a6c37bad>
2010-01-22 05:49:52 +0000

    adding in some stuff
    git-svn-id: https://posk.googlecode.com/svn/trunk@51 0a7cd2d0-06b7-11df-b5c4-2942a6c37bad

--- src/kernel_start.asm
+++ src/kernel_start.asm
@@ -1,5 +1,5 @@
-global loader           ; making entry point visible to linker
-extern posk_start            ; kmain is defined elsewhere
+global loader       ; making entry point visible to linker
+extern posk_start   ; posk_start is defined in posk.c
  
 ; setting up the Multiboot header - see GRUB docs for details
 MODULEALIGN equ  1<<0                   ; align loaded modules on page boundaries
@@ -7,7 +7,7 @@ MEMINFO     equ  1<<1                   ; provide memory map
 FLAGS       equ  MODULEALIGN | MEMINFO  ; this is the Multiboot 'flag' field
 MAGIC       equ    0x1BADB002           ; 'magic number' lets bootloader find the header
 CHECKSUM    equ -(MAGIC + FLAGS)        ; checksum required
- 
+
 section .text
 align 4
 MultiBootHeader:
@@ -22,12 +22,11 @@ loader:
    mov esp, stack+STACKSIZE           ; set up the stack
    push eax                           ; pass Multiboot magic number
    push ebx                           ; pass Multiboot info structure
- 
    call  posk_start                   ; call kernel proper
- 
    cli
+
 hang:
-   hlt                                ; halt machine should kernel return
+   hlt   ; halt machine should kernel return
    jmp   hang
  
 section .bss
