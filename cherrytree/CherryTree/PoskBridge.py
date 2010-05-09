5144c97e5c2dae9ce59b08c7a3596e01989c6be2
paultag <paultag@0a7cd2d0-06b7-11df-b5c4-2942a6c37bad>
2010-05-09 22:17:24 +0000

    whoot
    git-svn-id: https://posk.googlecode.com/svn/trunk@214 0a7cd2d0-06b7-11df-b5c4-2942a6c37bad

--- cherrytree/CherryTree/PoskBridge.py
+++ cherrytree/CherryTree/PoskBridge.py
@@ -2,13 +2,13 @@ import CherryTree.CherryTreeGlobals
 
 CHERRYTREE_CURRENT_PID = 0
 
-def createProcess( time, resources, priority ):
+def createProcess( time, resources, priority, name ):
 	global CHERRYTREE_CURRENT_PID
 	CHERRYTREE_CURRENT_PID += 1
 
 	if CherryTree.CherryTreeGlobals.DEBUGGING == 1:
-		print "d_printk(\"Creating new process ( %d )\\n\", " + str(CHERRYTREE_CURRENT_PID) + ");"
+		print "d_printk(\"Creating new process ( %d ) -- %s\\n\", " + str(CHERRYTREE_CURRENT_PID) + ", \"" + name + "\");"
 
-	print "newTask( " + str(CHERRYTREE_CURRENT_PID) + ", " + str(time) + ", " + str(resources) + ", " + str(priority) + " );"
+	print "newTask( " + str(CHERRYTREE_CURRENT_PID) + ", " + str(time) + ", " + str(resources) + ", " + str(priority) + " ); // " + name
 
 
--- cherrytree/CherryTree/__init__.py
+++ cherrytree/CherryTree/__init__.py
@@ -15,7 +15,7 @@ class CherryTree:
 					resources = data['resources']
 					rMask = 0 # ToDo: Fixme
 					global RESOURCE
-					PoskBridge.createProcess( data['time'], rMask, data['priority'] )
+					PoskBridge.createProcess( data['time'], rMask, data['priority'], active )
 				except KeyError as e:
 					sys.stderr.write( "\n** Critical Error **\nMalformed config file. Check `" + active + "`\n\n" )
 					sys.exit(-1)
