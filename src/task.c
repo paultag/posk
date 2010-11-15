c81e949bdcc1714ad2dc123cc6cd77934558af81
Paul Tagliamonte <paultag@ubuntu.com>
2010-11-14 20:42:39 -0500

    more indenting etx

--- src/task.c
+++ src/task.c
@@ -12,86 +12,86 @@ static void task_exit();
 static void sleep_exit();
 
 task_t * init_threading() {
-  task_t * task = (task_t*)kmalloc( sizeof(task_t) );
-  task->tid = next_tid++;
+	task_t * task = (task_t*)kmalloc( sizeof(task_t) );
+	task->tid = next_tid++;
   
-  current_task = task;
+	current_task = task;
   
-  return task;
+	return task;
 }
 
 int dummy_func(uint32_t iter, uint32_t tid) {
-  while(iter > 0) {
-	printk("exec tid %d\n", tid);
-	iter--;
-    }
-    return 0;
+	while(iter > 0) {
+		printk("exec tid %d\n", tid);
+		iter--;
+	}
+	return 0;
 }
 
 int ksleep_func(uint32_t time) {
-    while(time > 0) {
-	printk("sleeping\n");
-	time--;
-    }
-    return 0;
+	while(time > 0) {
+		printk("sleeping\n");
+		time--;
+	}
+	return 0;
 }
 
 task_t * create_dumb_task(int32_t time, uint32_t pnumber, uint32_t * stack) {
-   task_t * task = (task_t*)kmalloc( sizeof(task_t) );
-   memset(task, 0, sizeof(task_t));
-   task->tid = next_tid++;
+	task_t * task = (task_t*)kmalloc( sizeof(task_t) );
+	memset(task, 0, sizeof(task_t));
+	task->tid = next_tid++;
    
-   *--stack = pnumber;
-   *--stack = time;
-   *--stack = (uint32_t)&task_exit;
-   *--stack = (uint32_t)&dummy_func;
-   *--stack = 0;
-   task->ebp = (uint32_t)stack;
-   task->esp = (uint32_t)stack;
+	*--stack = pnumber;
+	*--stack = time;
+	*--stack = (uint32_t)&task_exit;
+	*--stack = (uint32_t)&dummy_func;
+	*--stack = 0;
+	task->ebp = (uint32_t)stack;
+	task->esp = (uint32_t)stack;
    
-   return task;
+	return task;
 }
 
 task_t * ksleep(int32_t time, uint32_t * stack) {
-   task_t * task = (task_t*)kmalloc( sizeof(task_t) );
-   memset(task, 0, sizeof(task_t));
-   task->tid = next_tid++;
+	task_t * task = (task_t*)kmalloc( sizeof(task_t) );
+	memset(task, 0, sizeof(task_t));
+	task->tid = next_tid++;
    
-   *--stack = time;
-   *--stack = (uint32_t)&sleep_exit;
-   *--stack = (uint32_t)&ksleep_func;
-   *--stack = 0;
-   task->ebp = (uint32_t)stack;
-   task->esp = (uint32_t)stack;
+	*--stack = time;
+	*--stack = (uint32_t)&sleep_exit;
+	*--stack = (uint32_t)&ksleep_func;
+	*--stack = 0;
+	task->ebp = (uint32_t)stack;
+	task->esp = (uint32_t)stack;
    
-   return task;
+	return task;
 }
 
 void switch_task(task_t * next) {
   
-  asm volatile ("mov %%esp, %0" : "=r" (current_task->esp));
-  asm volatile ("mov %%ebp, %0" : "=r" (current_task->ebp));
-  asm volatile ("mov %%ebx, %0" : "=r" (current_task->ebx));
-  asm volatile ("mov %%esi, %0" : "=r" (current_task->esi));
-  asm volatile ("mov %%edi, %0" : "=r" (current_task->edi));
+	asm volatile ("mov %%esp, %0" : "=r" (current_task->esp));
+	asm volatile ("mov %%ebp, %0" : "=r" (current_task->ebp));
+	asm volatile ("mov %%ebx, %0" : "=r" (current_task->ebx));
+	asm volatile ("mov %%esi, %0" : "=r" (current_task->esi));
+	asm volatile ("mov %%edi, %0" : "=r" (current_task->edi));
 
-  current_task = next;
+	current_task = next;
 
-  asm volatile ("mov %0, %%edi" : : "r" (next->edi));
-  asm volatile ("mov %0, %%esi" : : "r" (next->esi));
-  asm volatile ("mov %0, %%ebx" : : "r" (next->ebx));
-  asm volatile ("mov %0, %%esp" : : "r" (next->esp));
-  asm volatile ("mov %0, %%ebp" : : "r" (next->ebp));
+	asm volatile ("mov %0, %%edi" : : "r" (next->edi));
+	asm volatile ("mov %0, %%esi" : : "r" (next->esi));
+	asm volatile ("mov %0, %%ebx" : : "r" (next->ebx));
+	asm volatile ("mov %0, %%esp" : : "r" (next->esp));
+	asm volatile ("mov %0, %%ebp" : : "r" (next->ebp));
 }
 
 void task_exit() {
-    register uint32_t val asm ("eax");
-    printk("thread exited with value %d\n", val);
-    for(;;);
+	register uint32_t val asm ("eax");
+	printk("thread exited with value %d\n", val);
+	for(;;);
 }
 
 void sleep_exit() {
-    register uint32_t val asm ("eax");
-    printk("done sleeping\n");
-    for(;;);
+	register uint32_t val asm ("eax");
+	printk("done sleeping\n");
+	for(;;);
 }
