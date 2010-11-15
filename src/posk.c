9149b4be59e961d0b1bf46b502f4871c93ffaa1a
Paul Tagliamonte <paultag@ubuntu.com>
2010-11-14 20:54:53 -0500

    main header fail

--- src/posk.c
+++ src/posk.c
@@ -1,39 +1,39 @@
 /**
-   * Kernel entry point, and main source file.
-   * @file posk.c
-   * @mainpage P.O.S.K.
-   * This is POSK, the teaching kernel. It's written from scratch
-   * in an effort to help educate future students of John Carroll
-   * on the innermost workings of the lowest level program running
-   * on every computer on the planet.
-   * 
-   * Although this kernel is aimed to future JCU students, it's a 
-   * well documented and clear example of a basic kernel. This is a 
-   * logical starting spot for anyone wishing to start learning about
-   * how a kernel works, or developing their own kernel.
-   *
-   * This kernel is intended to be less then streamlined. A lot of 
-   * routines have been written to be clear, and not fast. Most 
-   * good kernel hackers can improve this kernel by a few orders
-   * of magnitudes. Feel free to do so, it is a great exercise. 
-   *
-   * We love POSIX, but this kernel will never be POSIX compliant. We will
-   * try our damnedest to get it working with a GNU userland, but no promises.
-   * 
-   * POSK is maintained and authored by Paul Tagliamonte <ptagliamonte11@jcu.edu>
-   * and Pat McDonnell <pmcdonnell10@jcu.edu>, Copyright (c) 2010.
-   *
-   * See http://www.jcu.edu/ for more information on John Carroll
-   * univeristy. Contact <dpalmer@jcu.edu> or <kirsch@jcu.edu> for
-   * more information on the John Carroll Computer Science Department.
-   *
-   * A large part of this kernel is based on the excellent resources
-   * compiled by James Molloy, available from http://www.jamesmolloy.co.uk/,
-   * as well as the resources available on http://osdev.org/
-   *
-   * Also, thanks to the OS gurus on Freenode's #osdev IRC channel.
-   *  
-   */
+ * Kernel entry point, and main source file.
+ * @file posk.c
+ * @mainpage P.O.S.K.
+ * This is POSK, the teaching kernel. It's written from scratch
+ * in an effort to help educate future students of John Carroll
+ * on the innermost workings of the lowest level program running
+ * on every computer on the planet.
+ * 
+ * Although this kernel is aimed to future JCU students, it's a 
+ * well documented and clear example of a basic kernel. This is a 
+ * logical starting spot for anyone wishing to start learning about
+ * how a kernel works, or developing their own kernel.
+ *
+ * This kernel is intended to be less then streamlined. A lot of 
+ * routines have been written to be clear, and not fast. Most 
+ * good kernel hackers can improve this kernel by a few orders
+ * of magnitudes. Feel free to do so, it is a great exercise. 
+ *
+ * We love POSIX, but this kernel will never be POSIX compliant. We will
+ * try our damnedest to get it working with a GNU userland, but no promises.
+ * 
+ * POSK is maintained and authored by Paul Tagliamonte <ptagliamonte11@jcu.edu>
+ * and Pat McDonnell <pmcdonnell10@jcu.edu>, Copyright (c) 2010.
+ *
+ * See http://www.jcu.edu/ for more information on John Carroll
+ * univeristy. Contact <dpalmer@jcu.edu> or <kirsch@jcu.edu> for
+ * more information on the John Carroll Computer Science Department.
+ *
+ * A large part of this kernel is based on the excellent resources
+ * compiled by James Molloy, available from http://www.jamesmolloy.co.uk/,
+ * as well as the resources available on http://osdev.org/
+ *
+ * Also, thanks to the OS gurus on Freenode's #osdev IRC channel.
+ *  
+ */
 
 // Now that you've read all that official legal mumbo-jumbo above,
 // here's the real story:
